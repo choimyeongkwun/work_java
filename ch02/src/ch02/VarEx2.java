24f367abb28f653bd70734df62f2d6e70676c1f3
KOITT <KOITT@KOITT-PC>
2018-05-23 21:39:33 +0900

    34쪽 예제

--- ch02/src/ch02/VarEx2.java
+++ ch02/src/ch02/VarEx2.java
@@ -2,22 +2,4 @@ package ch02;
 
 public class VarEx2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x = 10, y = 20; // 변수 x, y를 선언하고, 각각 10과 20으로 초기화
-		int tmp = 0; 	// 변수 tmp를 선언하고, 0으로 초기화
-		
-		System.out.println("x:" + x + "y:" + y); // x와 y값 출력
-		
-		tmp = x; // x의 값을 tmp 변수에 저장
-		x = y; // x 변수에 y값을 저장
-		y = tmp; // y변수에 tmp  값을 저장
-		
-		System.out.println("x:" + x + "y:" + y); // x와 y값 출력
-		System.out.println( x * y);
-		/*
-		 *  이 코드의 목적 : x와 y값을 바꿔치기 하는 방법
-		 */
-	}
-
 }
