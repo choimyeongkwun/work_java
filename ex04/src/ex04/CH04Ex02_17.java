2eb690bb3a750178681f1f7f1a88eb0ba082aee9
KOITT <KOITT@KOITT-PC>
2018-06-15 21:11:19 +0900

    배열

--- ex04/src/ex04/CH04Ex02_17.java
+++ ex04/src/ex04/CH04Ex02_17.java
@@ -7,18 +7,21 @@ public static void main(String []args) {
 	Scanner scanner = new Scanner(System.in);
 	int num = Integer.parseInt(scanner.nextLine());
 	
+	
+	//숫자를 출력할 변수 선언 및 초기화
 	int count = 1;
-	for(int i = 1 ; i<=num; i++) {
+	// 틀 작성
+	for(int i = 0;i<num; i++) {
 		// 대각선 왼쪽: 공백 출력 0 1 2 3개
-		for(int j =0; j<i-1; j++) {
-			System.out.print("*");
-			} 
-		 for(int i =1; i<num; i++){
-			 for(int j =1; j<num-1; j++) {
-				 System.out.print(count);
+		for(int j =0; j<num; j++) {
+			if(j>=i) {
+			System.out.print((count % 10)+" ");
 			count++;
+			}else {
+				System.out.print("  ");
 			}
-		 }
+		}
+		
 		 System.out.println();
 	
 	}
---
+++ ex04/src/ex04/Ch04Ex02_18.java
@@ -0,0 +1,34 @@
+package ex04;
+
+import java.util.Scanner;
+
+public class Ch04Ex02_18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 자연수 하나 입력받기
+		Scanner scanner = new Scanner(System.in);
+		int num = Integer.parseInt(scanner.nextLine());
+
+		// 2. 틀 작성
+		for (int i = 0; i < num * 2 - 1; i++) {
+			if (i < num) {
+				for(int j =0; j<=i; j++) {
+					System.out.print("#");
+				}
+
+			} else {
+				for (int j = 0; j < num; j++) {
+					int limit = i%num+1;
+					if(j < limit) {
+						System.out.print(" ");
+					}else {
+					System.out.print("#");
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
